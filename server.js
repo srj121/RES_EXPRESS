222c2f2dce288871f9d87ad5bc1f7c3ceb40964b
srj121 <surajbade121@gmail.com>
2023-06-20 00:57:02 +0530

    profile photo

--- server.js
+++ server.js
@@ -4,12 +4,37 @@ const cors = require("cors");
 app.use(cors());
 require("dotenv").config();
 
+// Adjust the limit of the request payload
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
+// Increase the server timeout if needed
+app.timeout = 120000; // 2 minutes in milliseconds
+
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000' || 'srj121.github.io/RES/');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 const userRouter = require("./routes/userRoutes");
 const authRouter = require("./routes/authRoutes");
 const extra = require("./routes/extraRoutes")
 const elasticEmail = require("./routes/elasticEmailRoutes")
 const verifyToken = require("./routes/varifyTokenRoutes")
+
+app.use((err, req, res, next) => {
+  // Handle payload size limit exceeded error
+  if (err instanceof SyntaxError && err.status === 413 && 'body' in err) {
+    res.status(413).json({ error: 'Payload size limit exceeded' });
+  } else {
+    // Handle other errors
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 const { connectToDatabase } = require("./configurations/db");
 
