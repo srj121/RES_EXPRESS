c0211a726328e4e9288c00127eb08106f6eaaacb
srj121 <suraj121@gmail.com>
2023-06-20 19:40:14 +0530

    verify Token

--- middleWare/verifyToken.js
+++ middleWare/verifyToken.js
@@ -6,17 +6,14 @@ const verifyToken = (req, res, next) => {
 const authToken ="Bearer "+ req.body.userKey;
   
 // const authToken = req.headers.authorization || req.headers.Authorization;
-console.log(authToken)
 let token;
 if (authToken && authToken.startsWith('Bearer')) {
   token = authToken.split(' ')[1];
-  console.log(token)
 }
 if (!token) {
   return res.status(401).json({ message: 'Missing token' });
 }
 jwt.verify(token, process.env.ACESS_KEY, (err, decoded) => {
-  console.log(decoded)
 
   if (err) {
     return res.status(401).json({ message: 'Invalid token' });
